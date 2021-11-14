3d7e8d675c3f3131bd4caa8f2ff3c7c8418f8e0a
ahmedchouchen <ahmedchouchen216@gmail.com>
2021-11-14 21:21:36 +0100

    mise a jour de fichier homework.java a partir du dossier clone

--- homework.java
+++ homework.java
@@ -2,5 +2,9 @@ public class homework{
     private int homework_id ;
     private String homework_name ;
     private String howork_time ;
-
+    
+    public void print(){
+         // TBD
+    }
+   
 }
